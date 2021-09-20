3e06e741f9d277edc51a820bd7fada80ee6c2667
Chayan-pro <cschayan4@gmail.com>
2021-09-20 21:39:35 +0600

    all done

--- src/App.js
+++ src/App.js
@@ -2,24 +2,64 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const cinemas =[
+    {nayok :'kuber', nayika: 'kopila'},
+    {nayok :'Laxman', nayika: 'Antora'},
+    {nayok :'Sujon', nayika: 'Toma'},
+    {nayok :'Jasim', nayika: 'suchorita'},
+    {nayok :'Chayan', nayika: 'Suborna'}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        cinemas.map(cinema =><Cinema nayok={cinema.nayok} nayika ={cinema.nayika}></Cinema>)
+      }
+      {/* <Person name="Shakib al hasan" profession ="Criket"></Person>
+      <Person name="Sujon" profession ="Football"></Person>
+      <Person name="Laxman" profession ="Teacher"></Person>
+      <Friend name="Suborna Sarker" Address ="Natore"></Friend>
+      <Friend name="Joye Roy" Address="Mohendronagar"></Friend>
+      <Friend name="Borsha Ray" Address="Pabna"></Friend> */}
     </div>
   );
 }
+
+function Person(props) {
+  const person ={
+    backgroundColor : 'pink',
+    color: 'white',
+    margin: '20px',
+    borderRadius: '20px'
+  }
+  return (
+    <div style={person}>
+      <h1>Name : {props.name}</h1>
+      <h4>Profession :{props.profession}</h4>
+    </div>
+  );
+}
+function Cinema (props){
+  return(
+    <div className ="person">
+      <h2>Nayok : {props.nayok}</h2>
+      <h4>Nayok : {props.nayika}</h4>
+    </div>
+  )
+}
+
+function Friend (props ){
+  const friendStyle = {
+    backgroundColor: 'orange',
+    color: 'balck',
+    fontSize : '20px',
+    margin : '20px'
+  }
+  return(
+    <div style ={friendStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Address:{props.Address}</p>
+    </div>
+  );
+};
 
 export default App;
